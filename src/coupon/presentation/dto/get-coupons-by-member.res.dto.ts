e0760a18ddf346f3d7f477b3ba0b7af74aef07da
psyoongsc <sh__y_@naver.com>
2025-04-03 23:22:25 +0900

    Feat: 쿠폰 컨트롤러

---
+++ src/coupon/presentation/dto/get-coupons-by-member.res.dto.ts
@@ -0,0 +1,13 @@
+import { MemberCouponResult } from "@app/coupon/domain/dto/member_coupon.result";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class GetCouponByMemberResDto {
+  @ApiProperty({
+    example: [
+      { id: 1, memberId: 1, couponId: 1, couponName: "10% 할인 쿠폰", offFigure: 10, isUsed: false },
+      { id: 2, memberId: 1, couponId: 2, couponName: "15% 할인 쿠폰", offFigure: 15, isUsed: true },
+    ],
+    description: "보유 쿠폰 리스트",
+  })
+  coupons: MemberCouponResult[];
+}
