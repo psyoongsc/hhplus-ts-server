a1155d6e964dec51584d3e5f4fc333bcaef925ec
psyoongsc <sh__y_@naver.com>
2025-04-07 20:34:34 +0900

    Fix: MemberRepository 비동기 처리 누락 보완

--- src/member/domain/memeber.repository.ts
+++ src/member/domain/memeber.repository.ts
@@ -1,6 +1,5 @@
 import { PrismaRepository } from "@app/database/prismaRepository.impl";
 import { Member } from "./entity/member.entity";
-import { PrismaClient } from "@prisma/client";
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "@app/database/prisma/prisma.service";
 
@@ -11,6 +10,6 @@ export class MemberRepository extends PrismaRepository<Member> {
   }
 
   async updateBalance(id: number, balance: number): Promise<Member> {
-    return this.updateById(id, { balance });
+    return await this.updateById(id, { balance });
   }
 }
