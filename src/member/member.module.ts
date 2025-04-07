87aaa5925cfebf08ff395d99c4986e80a91d69dd
psyoongsc <sh__y_@naver.com>
2025-04-07 13:58:45 +0900

    Feat: 회원 관리 모듈에 Prisma Service 주입

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { MemberService } from "./domain/service/member.service";
 import { MemberController } from "./presentation/member.controller";
 import { MemberRepository } from "./domain/memeber.repository";
+import { PrismaModule } from "@app/database/prisma/prisma.module";
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MemberController],
   providers: [MemberService, MemberRepository],
 })
