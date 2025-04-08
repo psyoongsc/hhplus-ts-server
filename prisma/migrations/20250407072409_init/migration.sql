e44eb49dc96c28b7d9dd57fdf725aded50611bf9
psyoongsc <sh__y_@naver.com>
2025-04-08 20:23:56 +0900

    Fix: ERD 변경 사항 반영 및 레포지토리 의존성 주입

--- prisma/migrations/20250407072409_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Product` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL DEFAULT '',
-    `stock` INTEGER NOT NULL DEFAULT 0,
-    `price` INTEGER NOT NULL DEFAULT 0,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/product/domain/product_sales_stat.repository.ts
+++ src/product/domain/product_sales_stat.repository.ts
@@ -1,7 +1,9 @@
 import { PrismaRepository } from "@app/database/prismaRepository.impl";
 import { Product_Sales_Stat } from "./entity/product_sales_stat.entity";
 import { PrismaService } from "@app/database/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
 export class ProductSalesStatRepository extends PrismaRepository<Product_Sales_Stat> {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma, prisma.product_Sales_Stat);
