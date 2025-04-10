ee71ffdf9a2904975766eec2e73ab7e858467898
psyoongsc <sh__y_@naver.com>
2025-04-11 04:19:54 +0900

    Feat: 결제 관리 서비스 구현 및 단위 테스트 작성

---
+++ src/payment/domain/dto/process-payment.command.ts
@@ -0,0 +1,27 @@
+import { IsDate, IsInt, IsOptional, IsPositive, Max } from "class-validator";
+
+export class ProcessPaymentCommand {
+  @IsInt()
+  @IsPositive()
+  @Max(2_147_483_647)
+  orderId: number;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  @Max(2_147_483_647)
+  couponId: number;
+
+  @IsInt()
+  @IsPositive()
+  @Max(2_147_483_647)
+  memberId: number;
+
+  @IsDate()
+  approved_at: Date;
+
+  @IsInt()
+  @IsPositive()
+  @Max(2_147_483_647)
+  amount: number;
+}
---
+++ src/payment/domain/service/payment.service.ts
@@ -0,0 +1,40 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { PaymentRepository } from "../infrastructure/payment.repository";
+import { ProcessPaymentCommand } from "../dto/process-payment.command";
+import { PaymentResult } from "../dto/payment.result";
+import { IPAYMENT_REPOSITORY } from "../payment.repository.interface";
+import { PaymentStatus } from "../dto/payment-status.enum";
+
+@Injectable()
+export class PaymentService {
+  constructor(
+    @Inject(IPAYMENT_REPOSITORY)
+    private readonly paymentRepository: PaymentRepository,
+  ) {}
+
+  // Payment,Order 관계 1:0..1 -> 환불 한 결제 건에 대해서 재결제 불가
+  async processPayment(command: ProcessPaymentCommand): Promise<PaymentResult> {
+    const orderId = command.orderId;
+    const couponId = command.couponId;
+    const memberId = command.memberId;
+    const approved_at = new Date();
+    const amount = command.amount;
+
+    // orderId 를 이용해 검색하였을 때, 검색되는 내용이 있다면 이미 결제 이력이 있으므로 Error 반환해야 함.
+    const payment_record = await this.paymentRepository.find({
+      where: { orderId },
+    });
+    if (payment_record.length != 0) {
+      throw new Error("ALREADY_PREOCESSED");
+    }
+
+    return await this.paymentRepository.createPayment(
+      orderId,
+      couponId,
+      memberId,
+      amount,
+      approved_at,
+      PaymentStatus.PAYMENT_COMPLETED,
+    );
+  }
+}
