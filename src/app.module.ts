bf5f8494c7190cf0c2472ae8efb0fd5843571b1b
psyoongsc <sh__y_@naver.com>
2025-04-11 04:21:19 +0900

    Feat: 각 모듈에 DIP 적용 및 모듈 통합

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { MemberModule } from "./member/member.module";
 import { CouponModule } from "./coupon/coupon.module";
 import { ProductModule } from "./product/product.module";
 import { OrderModule } from "./order/order.module";
+import { ProductSalesStatModule } from "./productSalesStat/productSalesStat.module";
+import { PaymentModule } from "./payment/payment.module";
 
 @Module({
-  imports: [MemberModule, CouponModule, ProductModule, OrderModule],
+  imports: [MemberModule, CouponModule, ProductModule, OrderModule, ProductSalesStatModule, PaymentModule],
 })
 export class AppModule {}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,12 +2,19 @@ import { Module } from "@nestjs/common";
 import { ProductService } from "./domain/service/product.service";
 import { ProductController } from "./presentation/product.controller";
 import { PrismaModule } from "@app/database/prisma/prisma.module";
-import { ProductRepository } from "./domain/product.repository";
-import { ProductSalesStatRepository } from "./domain/product_sales_stat.repository";
+import { ProductRepository } from "./domain/infrastructure/product.repository";
+import { IPRODUCT_REPOSITORY } from "./domain/product.repository.interface";
 
 @Module({
   imports: [PrismaModule],
   controllers: [ProductController],
-  providers: [ProductService, ProductRepository, ProductSalesStatRepository],
+  providers: [
+    ProductService,
+    {
+      provide: IPRODUCT_REPOSITORY,
+      useClass: ProductRepository,
+    },
+  ],
+  exports: [ProductService],
 })
 export class ProductModule {}
