9cc48db066f8a447aaaf47395f33a1ba71ad72d0
psyoongsc <sh__y_@naver.com>
2025-04-16 17:15:45 +0900

    Fix: 쿠폰 검색 용이성을 위한 유니크 키 추가 및 불필요 migration 정리

--- prisma/migrations/20250412140647_modify_balance_history/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `createdAt` on table `Balance_History` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `Balance_History` MODIFY `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
