8cbd378fdceb2d9c4e5a84a58fa318540bfc420b
psyoongsc <sh__y_@naver.com>
2025-05-14 17:54:04 +0900

    Refactor: Redis를 이용한 인기 상품 서비스 모듈 등록

--- src/productSalesStat/productSalesStat.module.ts
+++ src/productSalesStat/productSalesStat.module.ts
@@ -7,21 +7,29 @@ import { IPRODUCT_SALES_STAT_REPOSITORY } from "./domain/repository/product_sale
 import { IPRODUCT_SALES_STAT_VIEW_REPOSITORY } from "./domain/repository/product_sales_stat_view.interface.repository";
 import { ProductSalesStatViewRepository } from "./infrastructure/product_sales_stat_view.repository";
 import { RedisModule } from "@app/redis/redis.module";
+import { IPRODUCT_SALES_STAT_REDIS_REPOSITORY } from "./domain/repository/product_sales_stat.redis.interface.repository";
+import { ProductSalesStatRedisRepository } from "./infrastructure/product_sales_stat.redis.repository";
+import { ProductSalesStatRedisService } from "./domain/service/productSalesStat.redis.service";
 
 @Module({
   imports: [PrismaModule, RedisModule],
   controllers: [ProductSalesStatController],
   providers: [
     ProductSalesStatService,
+    ProductSalesStatRedisService,
     {
       provide: IPRODUCT_SALES_STAT_REPOSITORY,
       useClass: ProductSalesStatRepository,
     },
+    {
+      provide: IPRODUCT_SALES_STAT_REDIS_REPOSITORY,
+      useClass: ProductSalesStatRedisRepository
+    }
     // {
     //   provide: IPRODUCT_SALES_STAT_VIEW_REPOSITORY,
     //   useClass: ProductSalesStatViewRepository,
     // },
   ],
-  exports: [ProductSalesStatService],
+  exports: [ProductSalesStatService, ProductSalesStatRedisService],
 })
 export class ProductSalesStatModule {}
