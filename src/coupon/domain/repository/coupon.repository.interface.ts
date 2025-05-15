b7fb08715fc5b6fa1146036b148527a325935e8d
psyoongsc <sh__y_@naver.com>
2025-05-16 00:59:53 +0900

    Feat: Redis 병행 사용을 위한 기존 Repository 기능 추가

--- src/coupon/domain/repository/coupon.repository.interface.ts
+++ src/coupon/domain/repository/coupon.repository.interface.ts
@@ -6,6 +6,7 @@ export interface ICouponRepository extends IRepository<Coupon> {
   getAllAvailableCoupons(tx?: Prisma.TransactionClient): Promise<Coupon[]>
   addCoupon(couponId: number, tx?: Prisma.TransactionClient): Promise<Coupon>;
   deductCoupon(couponId: number, tx?: Prisma.TransactionClient): Promise<Coupon>;
+  updateCouponStock(couponId: number, stock: number, tx?: Prisma.TransactionClient): Promise<Coupon>;
 }
 
 export const ICOUPON_REPOSITORY = Symbol("ICOUPON_REPOSITORY");
