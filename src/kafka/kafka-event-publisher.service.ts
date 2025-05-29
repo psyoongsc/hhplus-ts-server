594b6f5b67a424e87eb873b52199e7671eb4299f
psyoongsc <sh__y_@naver.com>
2025-05-29 10:32:50 +0900

    Refactor: 통합 테스트를 위한 Broker 설정 변경

--- src/kafka/kafka-event-publisher.service.ts
+++ src/kafka/kafka-event-publisher.service.ts
@@ -3,22 +3,33 @@ import { Kafka, Producer } from "kafkajs";
 
 @Injectable()
 export class KafkaEventPublisherService implements OnModuleInit, OnModuleDestroy {
-  private readonly producer: Producer;
+  private producer: Producer;
+  private isTestMode: boolean;
 
   constructor() {
-    const kafka = new Kafka({
-      clientId: 'ecommerce-server',
-      brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094'],
-    });
-    this.producer = kafka.producer();
+    this.isTestMode = process.env.KAFKA_TEST_MODE === 'true';
   }
 
   async onModuleInit() {
-      await this.producer.connect();
+    if(this.isTestMode) {
+      const kafka = new Kafka({
+        clientId: 'ecommerce-server',
+        brokers: [`${process.env.KAFKA_HOST}:${process.env.KAFKA_PORT}`]
+      })
+      this.producer = kafka.producer();
+    } else {
+      const kafka = new Kafka({
+        clientId: 'ecommerce-server',
+        brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094'],
+      });
+      this.producer = kafka.producer();
+    }
+
+    await this.producer.connect();
   }
 
   async onModuleDestroy() {
-      await this.producer.disconnect();
+    await this.producer.disconnect();
   }
 
   async publish(params: { topic: string; key: string; value: string }) {
