5b5eb8913c50e0e4c1456ed2e600c47815a22848
psyoongsc <sh__y_@naver.com>
2025-05-29 10:31:50 +0900

    Chore: Kafka 통합 테스트를 위한 패키지 설치 및 컨테이터 세팅

--- test/it/setup.ts
+++ test/it/setup.ts
@@ -1,11 +1,13 @@
 import { MySqlContainer, StartedMySqlContainer } from '@testcontainers/mysql';
 import { RedisContainer, StartedRedisContainer } from '@testcontainers/redis';
+import { KafkaContainer, StartedKafkaContainer } from '@testcontainers/kafka';
 import { execSync } from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 
 let mysql_container: StartedMySqlContainer;
 let redis_container: StartedRedisContainer;
+let kafka_container: StartedKafkaContainer;
 
 export default async () => {
   mysql_container = await new MySqlContainer('mysql:8')
@@ -44,8 +46,17 @@ export default async () => {
   process.env.REDIS_HOST = redisHost;
   process.env.REDIS_PORT = redisPort.toString();
 
+  // kafka container
+  kafka_container = await new KafkaContainer('confluentinc/cp-kafka:7.2.1').start();
+  const kafkaHost = kafka_container.getHost();
+  const kafkaPort = kafka_container.getMappedPort(9093);
+
+  process.env.KAFKA_TEST_MODE = 'true';
+  process.env.KAFKA_HOST = kafkaHost;
+  process.env.KAFKA_PORT = kafkaPort.toString();
 
   // Save container for teardown
   globalThis.__DB_CONTAINER__ = mysql_container;
   globalThis.__REDIS_CONTAINER__ = redis_container;
+  globalThis.__KAFKA_CONTAINER__ = kafka_container;
 };
