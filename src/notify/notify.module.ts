b9b7c119fb69b2c99591b392dda1f33987df7f2c
psyoongsc <sh__y_@naver.com>
2025-06-05 21:35:22 +0900

    Fix: 불필요 패키지 의존성 제거

--- src/notify/notify.module.ts
+++ src/notify/notify.module.ts
@@ -2,13 +2,11 @@ import { Module } from "@nestjs/common";
 import { NotifyService } from "./domain/service/notify.service";
 import { PayCompletedListener } from "./application/event-listener/pay-completed.listener";
 import { SendOrderInfoToExtPlatformUsecase } from "./application/send-order-info-to-ext-platform.usecase";
-import { CqrsModule } from "@nestjs/cqrs";
 import { KafkaModule } from "@app/kafka/kafka.module";
 import { NotifyPayCompletedConsumer } from "./infrastructure/notify-pay-completed.consumer";
 
 @Module({
   imports: [
-    CqrsModule, 
     KafkaModule
   ],
   controllers: [],
